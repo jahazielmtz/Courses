6048768c2cdf12fa96e7d519bf06c6973ac1f48d
Jahaziel Martinez <jahazielmartinez@gmail.com>
2017-10-26 19:10:52 -0500

    Updated course Angular4

--- Angular4/jazzosquare/src/app/app.component.ts
+++ Angular4/jazzosquare/src/app/app.component.ts
@@ -7,16 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'JazzoSquare';
-  a = 3;
-  b = 5;
-  listo = false;
-  nombre:string = '';
-
+  lugares:any = [
+    {plan: 'pagado', cercania: 1, distancia: 1, active: true, nombre: 'Florería La Gardenia'},
+    {plan: 'gratuito', cercania: 1, distancia: 1.8, active: false, nombre: 'Donas La Pasadita'},
+    {plan: 'gratuito', cercania: 2, distancia: 5, active: true, nombre: 'Veterinaria Huellitas Felices'},
+    {plan: 'gratuito', cercania: 3, distancia: 10, active: true, nombre: 'Florería La Gardenia 2'},
+    {plan: 'pagado', cercania: 3, distancia: 35, active: true, nombre: 'Donas La Pasadita 2'},
+    {plan: 'gratuito', cercania: 3, distancia: 120, active: false, nombre: 'Veterinaria Huellitas Felices 2'}
+  ];
+  lat:number = 20.5706569;
+  lng:number = -103.3483462;
   constructor() {
-    setTimeout(()=>{this.listo = true;}, 3000);
-  }
 
-  hacerAlgo() {
-    alert('Haciendo algo');
   }
 }
--- Angular4/jazzosquare/src/app/app.module.ts
+++ Angular4/jazzosquare/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-
+import { AgmCoreModule } from '@agm/core';
+import { ResaltarDirective } from './directives/resaltar.directive';
+import { ContarClicksDirective } from './directives/contar-clicks.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective,
+    ContarClicksDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCo-JSjZNGAlRDgz6JJ-B0yvJUMBjrsqAk'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
